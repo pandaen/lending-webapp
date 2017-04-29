0d26feee8838cd629595c623826dd6d7695a5aaa
ada010 <mirosagen@hotmail.com>
2017-04-29 18:04:37 +0200

    inital project with working angularfire2 demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from "angularfire2";
+//  Firebase Settings
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAHfCQArz_9VdSVJ0rGhaEMYeZuv8JJCIY',
+  authDomain: 'borrowing-app.firebaseapp.com',
+  databaseURL: 'https://borrowing-app.firebaseio.com',
+  storageBucket: 'borrowing-app.appspot.com',
+  messagingSenderId: '226399216748'
+};
+
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
