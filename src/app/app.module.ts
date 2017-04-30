71043e0e85dd2a9f5e45177e3d63b0979ea619eb
ada010 <mirosagen@hotmail.com>
2017-04-30 02:46:17 +0200

    Angularfire & layout working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {AngularFireModule} from 'angularfire2';
@@ -24,11 +23,9 @@ export const firebaseConfig = {
     FormsModule,
     ItemModule,
     AppRoutingModule,
-    HttpModule,
     AngularFireModule.initializeApp(firebaseConfig)],
   declarations: [
     AppComponent,
-    DemoPageComponent,
   ],
   bootstrap: [AppComponent]
 })
