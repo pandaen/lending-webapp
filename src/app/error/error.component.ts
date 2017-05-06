ddbe7be350d176614e8653d3d423409f5b6f6f5b
ada010 <mirosagen@hotmail.com>
2017-05-06 02:27:48 +0200

    page not found tweaks

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
+
+    template: `
+      <h1 class="errorMessage">404</h1>
+      <h3 class="sub"> page not found</h3>
+    `,
+    styles: [`
+      .errorMessage {
+        margin-top:150px;
+        font-size: 170px;
+        text-align: center;
+      }
+      .sub {
+        margin-top:20px;
+        font-size: 50px;
+        color: red;
+        text-align: center;
+      }
+    
+    `]
+
+  /*
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.css']
+  */
  })
 export class ErrorComponent { }
--- src/app/items/item-list.component.ts
+++ src/app/items/item-list.component.ts
@@ -3,30 +3,32 @@ import {UserService} from '../admin/adminShared/user.service';
 import {Router} from "@angular/router";
 
 @Component({
-    moduleId: module.id, // can now use realtive path (omit app/pages..)
-    templateUrl: 'item-list.component.html',
-    styleUrls: ['item-list.component.css']
+  moduleId: module.id, // can now use realtive path (omit app/pages..)
+  templateUrl: 'item-list.component.html',
+  styleUrls: ['item-list.component.css']
 })
 export class ItemListComponent implements OnInit {
-    pageTitle: string = 'Borrowing Admin panel';
-    imageWidth: number = 50;
-    imageMargin: number = 2;
-    listFilter: string;          // Set deafult search here
+  pageTitle: string = 'Borrowing Admin panel';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  listFilter: string;          // Set deafult search here
   items: any;
-theUser: string;
-userImage: string;
-id: any;
-    constructor( private uService: UserService) {
+  theUser: string;
+  userImage: string;
+  id: any;
+  filterBy: string = 'all';
 
-    }
+  constructor(private uService: UserService) {
 
-    ngOnInit(): void {
-      this.theUser = this.uService.loggedInUser;
-      this.userImage = this.uService.userImage;
-      this.uService.getItems().subscribe(items => {
-        this.items = items;
-      });
-    }
+  }
+
+  ngOnInit(): void {
+    this.theUser = this.uService.loggedInUser;
+    this.userImage = this.uService.userImage;
+    this.uService.getItems().subscribe(items => {
+      this.items = items;
+    });
+  }
 
   logout() {
     this.uService.logout();
